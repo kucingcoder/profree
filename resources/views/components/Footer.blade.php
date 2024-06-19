cb379dc0f69039653ef29e069abbcbaf235956df
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-06-20 05:07:07 +0700

    Desain footer

--- resources/views/components/Footer.blade.php
+++ resources/views/components/Footer.blade.php
@@ -1,1 +1,13 @@
-Disini Footer
+<footer class="content-footer footer bg-light">
+    <div class="container-xxl d-flex flex-wrap justify-content-center py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            ©
+            <script>
+                document.write(new Date().getFullYear());
+            </script>
+            , made with ❤️ by
+            <a href="https://github.com/kucingcoder" target="_blank" class="footer-link fw-bolder">M. Ibrahim Hanif</a>
+            from Politeknik Harapan Bersama
+        </div>
+    </div>
+</footer>
