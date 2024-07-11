73545feeadb1ddbeba12934f509e2b551217d64c
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2024-07-11 20:09:05 +0700

    Salah ambil data

--- app/Http/Controllers/Otentikas.php
+++ app/Http/Controllers/Otentikas.php
@@ -140,7 +140,7 @@ class Otentikas extends Controller
         if ($konfirmasi) {
             KonfirmasiEmail::where('pengguna_id', $konfirmasi)->delete();
 
-            $pengguna = Pengguna::find($konfirmasi);
+            $pengguna = Pengguna::where('pengguna_id', $konfirmasi)->first();
             if ($pengguna) {
                 $pengguna->status_akun_id = 2;
                 $pengguna->save();
